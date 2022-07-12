5a29933035725973ec0cc38544f5cb7a7b0003a2
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-07-12 11:27:43 +0200

    Person class added

--- SpringBootExercise/src/main/java/com/rest/SpringBootExercise/Controller.java
+++ SpringBootExercise/src/main/java/com/rest/SpringBootExercise/Controller.java
@@ -1,2 +1,17 @@
-package com.rest.SpringBootExercise;public class Controller {
+package com.rest.SpringBootExercise;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/c1")
+public class Controller {
+    @GetMapping("/user/{name}")
+    String getHola(@PathVariable String name){
+        return "Hola " + name;
+    }
+
+    @PostMapping("/useradd")
+    Employee newEmployee(@RequestBody Person requestPerson) {
+        return repository.save(newEmployee);
+    }
+
 }
