716ccca9901c6100e2917e924671175a8d597a7a
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-07-15 12:38:35 +0200

    BeansLifecycle proyect initialized

---
+++ BeansLifecycle/src/main/java/spring/springboot/BeansLifecycle/BeansLifecycleApplication.java
@@ -0,0 +1,13 @@
+package spring.springboot.BeansLifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansLifecycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansLifecycleApplication.class, args);
+	}
+
+}
---
+++ BeansLifecycle/src/test/java/spring/springboot/BeansLifecycle/BeansLifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springboot.BeansLifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansLifecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
