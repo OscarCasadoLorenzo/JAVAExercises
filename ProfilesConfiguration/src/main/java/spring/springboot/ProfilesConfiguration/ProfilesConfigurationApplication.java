4b35bcd78c9bada158b0b0862ff6573064c84d65
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-07-19 13:45:59 +0200

    ProfilesConfiguration proyect initialized

---
+++ ProfilesConfiguration/src/main/java/spring/springboot/ProfilesConfiguration/ProfilesConfigurationApplication.java
@@ -0,0 +1,13 @@
+package spring.springboot.ProfilesConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesConfigurationApplication.class, args);
+	}
+
+}
---
+++ ProfilesConfiguration/src/test/java/spring/springboot/ProfilesConfiguration/ProfilesConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springboot.ProfilesConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
