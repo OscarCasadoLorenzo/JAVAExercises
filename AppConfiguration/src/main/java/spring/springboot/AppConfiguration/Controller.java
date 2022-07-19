8fc19f138a3c3f53c6361fdde4c6f8d53712fca4
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-07-19 13:30:16 +0200

    Configuration with application.properties OK

--- AppConfiguration/src/main/java/spring/springboot/AppConfiguration/Controller.java
+++ AppConfiguration/src/main/java/spring/springboot/AppConfiguration/Controller.java
@@ -1,4 +1,20 @@
 package spring.springboot.AppConfiguration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/c1")
 public class Controller {
+    @Value("${VAR1}")
+    String var1;
+    @Value("${MY.VAR2}")
+    String var2;
+    @Value("${VAR3:'VAR3 doesnt have any value'}")
+    String var3;
+
+    @GetMapping("/values")
+    public String getValues(){
+        return "Value of VAR1 is " + var1 + " and value of MY.VAR2 is " + var2 + "\nValue of VAR3 is " + var3;
+    }
 }
