2b1042e4d9f0311246be33d9713ef11e81798a83
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-07-22 12:41:23 +0200

    validAndException proyect initialized

---
+++ ValidAndException/src/test/java/spring/springboot/ValidAndException/ValidAndExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springboot.ValidAndException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidAndExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
