4bda0b7c8aedbc0501c8b101ec8814bc3052ff6c
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-08-01 12:29:27 +0200

    CORS in PersonController.java

--- TableRelations/src/main/java/spring/springboot/TableRelations/Person/infraestructure/controller/PersonController.java
+++ TableRelations/src/main/java/spring/springboot/TableRelations/Person/infraestructure/controller/PersonController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/person")
+@CrossOrigin(origins = "*")
 public class PersonController {
     @Autowired
     PersonService personService;
