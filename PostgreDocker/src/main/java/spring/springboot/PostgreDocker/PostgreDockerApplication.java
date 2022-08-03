e188919b05799beb6cb0ba815d76c641ca7253fb
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-08-03 09:13:35 +0200

    Docker's PostgreSQL connection proyect initialized

---
+++ PostgreDocker/src/main/java/spring/springboot/PostgreDocker/PostgreDockerApplication.java
@@ -0,0 +1,13 @@
+package spring.springboot.PostgreDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreDockerApplication.class, args);
+	}
+
+}
---
+++ PostgreDocker/src/test/java/spring/springboot/PostgreDocker/PostgreDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springboot.PostgreDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
