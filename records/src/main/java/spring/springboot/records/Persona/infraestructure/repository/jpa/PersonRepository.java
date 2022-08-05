ffc83e8699fd3726ed318dad629c82a082e225d3
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-08-05 09:05:43 +0200

    Files imported from EJ2(JPAExmaple)

---
+++ records/src/main/java/spring/springboot/records/Persona/infraestructure/repository/jpa/PersonRepository.java
@@ -0,0 +1,18 @@
+package spring.springboot.records.Persona.infraestructure.repository.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+import spring.springboot.JPAExample.Persona.domain.PersonEntity;
+
+import java.util.List;
+
+@Service
+public interface PersonRepository extends CrudRepository<PersonEntity, Integer> {
+    /*  RETURN A PERSONENTITY OR A PERSONOUTPUTDTO?
+        It is a good practise to return the Entity object, because
+        if in the future we would change a class's property it won't
+        do the automatic conversion.
+    */
+    List<PersonEntity> findByName(String name);
+
+}
