a9a70a9c8cb88d6dc09dd5aac227a8ea88dce956
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-08-10 09:57:21 +0200

    FileManagement proyect initialized

---
+++ FileManagement/src/test/java/spring/springboot/FileManagement/FileManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springboot.FileManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
