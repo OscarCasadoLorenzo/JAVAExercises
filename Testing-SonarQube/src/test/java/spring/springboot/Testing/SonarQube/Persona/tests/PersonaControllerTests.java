f9cce10f7787e070b42d3ec03c3629062847b0ce
Oscar Casado <oscar.clorenzo@bosonit.com>
2022-08-22 10:24:46 +0200

    Test file created

---
+++ Testing-SonarQube/src/test/java/spring/springboot/Testing/SonarQube/Persona/tests/PersonaControllerTests.java
@@ -0,0 +1,8 @@
+package spring.springboot.Testing.SonarQube.Persona.tests;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PersonaControllerTests {
+}
