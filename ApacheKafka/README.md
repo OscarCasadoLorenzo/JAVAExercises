bfb77916c569bccc43d07e97be5212770fd93513
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2022-08-29 09:34:42 +0200

    README topic renamed

--- ApacheKafka/README.md
+++ ApacheKafka/README.md
@@ -9,10 +9,10 @@ Start the ZooKeeper service
 .\bin\windows\kafka-server-start.bat .\config\server.properties
 
 #### STEP 3: CREATE A TOPIC TO STORE YOUR EVENTS  
-.\bin\windows\kafka-topics.bat --create --topic topic_demo --bootstrap-server localhost:9092
+.\bin\windows\kafka-topics.bat --create --topic javaguides --bootstrap-server localhost:9092
 
 #### STEP 4: WRITE SOME EVENTS INTO THE TOPIC  
-.\bin\windows\kafka-console-producer.bat --topic topic_demo --bootstrap-server localhost:9092
+.\bin\windows\kafka-console-producer.bat --topic javaguides --bootstrap-server localhost:9092
 
 #### STEP 5:  READ THE EVENTS
-.\bin\windows\kafka-console-consumer.bat --topic topic_demo --from-beginning --bootstrap-server localhost:9092
+.\bin\windows\kafka-console-consumer.bat --topic javaguides --from-beginning --bootstrap-server localhost:9092
--- ApacheKafka/src/main/java/spring/springboot/ApacheKafka/controller/MessageController.java
+++ ApacheKafka/src/main/java/spring/springboot/ApacheKafka/controller/MessageController.java
@@ -4,20 +4,18 @@ import org.apache.coyote.Response;
 import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.springboot.ApacheKafka.kafka.KafkaProducer;
 
 @RestController
-@RequestMapping("api/v1/kafka")
+@RequestMapping("kafka/guidelines")
 public class MessageController {
 
     @Autowired
     KafkaProducer kafkaProducer;
 
-    @GetMapping("")
-    public ResponseEntity<String> publish(String message){
+    @PostMapping
+    public ResponseEntity<String> publish(@RequestParam String message){
         kafkaProducer.sendMessage(message);
         return ResponseEntity.ok("Message was sent to the topic");
     }
