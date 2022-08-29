731fb06708944ddc325181e9fe8d811158853f95
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2022-08-29 09:43:30 +0200

    Changes on logger message

--- ApacheKafka/src/main/java/spring/springboot/ApacheKafka/kafka/KafkaProducer.java
+++ ApacheKafka/src/main/java/spring/springboot/ApacheKafka/kafka/KafkaProducer.java
@@ -1,6 +1,5 @@
 package spring.springboot.ApacheKafka.kafka;
 
-import org.apache.kafka.common.protocol.types.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
