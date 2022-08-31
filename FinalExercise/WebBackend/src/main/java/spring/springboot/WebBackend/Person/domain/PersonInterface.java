d10d31722c5e2ca899596de2cc919f593d52daf5
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2022-08-31 13:57:11 +0200

    PersonService and PersonInterface implemented

--- FinalExercise/WebBackend/src/main/java/spring/springboot/WebBackend/Person/domain/PersonInterface.java
+++ FinalExercise/WebBackend/src/main/java/spring/springboot/WebBackend/Person/domain/PersonInterface.java
@@ -1,7 +1,22 @@
 package spring.springboot.WebBackend.Person.domain;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import spring.springboot.WebBackend.Person.infraestructure.controller.dto.input.PersonInputDTO;
+import spring.springboot.WebBackend.Person.infraestructure.controller.dto.output.PersonOutputDTO;
+
+import java.util.List;
+
 public interface PersonInterface {
 
+    List<PersonOutputDTO> getAllPersons();
 
+    boolean existsPerson(int id);
 
+    PersonOutputDTO getPersonByID(int id);
+
+    PersonOutputDTO postPerson(PersonInputDTO personInputDTO);
+
+    PersonOutputDTO updatePerson(int id, PersonInputDTO personaInputDTO);
+
+    PersonOutputDTO deletePerson(int id);
 }
--- FinalExercise/WebBackend/src/main/java/spring/springboot/WebBackend/Person/domain/PersonService.java
+++ FinalExercise/WebBackend/src/main/java/spring/springboot/WebBackend/Person/domain/PersonService.java
@@ -2,12 +2,71 @@ package spring.springboot.WebBackend.Person.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+import spring.springboot.WebBackend.Person.infraestructure.controller.dto.input.PersonInputDTO;
+import spring.springboot.WebBackend.Person.infraestructure.controller.dto.output.PersonOutputDTO;
 import spring.springboot.WebBackend.Person.infraestructure.repository.PersonRepository;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class PersonService {
+public class PersonService implements PersonInterface{
     @Autowired
     PersonRepository personRepository;
 
+    @Override
+    public List<PersonOutputDTO> getAllPersons() {
+        List<PersonOutputDTO> personaOutputDTOList = new ArrayList<>();
+
+        for (PersonEntity personEntity : personRepository.findAll()){
+            PersonOutputDTO auxOutputDTO = new PersonOutputDTO(personEntity);
+            personaOutputDTOList.add(auxOutputDTO);
+        }
+
+        return personaOutputDTOList;
+    }
+
+    @Override
+    public boolean existsPerson(int id) {
+        return personRepository.existsById(id);
+    }
+
+    @Override
+    public PersonOutputDTO getPersonByID(int id) {
+        PersonEntity personEntity = personRepository.findById(id).orElse(null);
+        PersonOutputDTO personaOutputDTO = new PersonOutputDTO(personEntity);
+        return personaOutputDTO;
+    }
+
+    @Override
+    public PersonOutputDTO postPerson(PersonInputDTO personInputDTO){
+
+        PersonEntity personEntity = new PersonEntity(personInputDTO);
+        personRepository.save(personEntity);
+        PersonOutputDTO personaOutputDTO = new PersonOutputDTO(personEntity);
+        return personaOutputDTO;
+
+    }
+
+    @Override
+    public PersonOutputDTO updatePerson(int id, @Valid PersonInputDTO personaInputDTO){
+        /*
+            We could then simply get the entity from the database,
+            make the change, and use the save() method as before.
+         */
+        PersonEntity personInDB = personRepository.findById(id).orElse(null);
+        personInDB.updateEntity(personaInputDTO);
+        personRepository.save(personInDB);
+
+        PersonOutputDTO personaOutputDTO = new PersonOutputDTO(personInDB);
+        return personaOutputDTO;
+    }
+
+    @Override
+    public PersonOutputDTO deletePerson(int id) {
+        PersonOutputDTO personaOutputDTO = getPersonByID(id);
+        personRepository.deleteById(id);
+        return personaOutputDTO;
+    }
 }
