3a491c0efe82094541edb19fd0724d8f0384bf62
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2022-09-22 12:21:33 +0200

    UnprocesableTripCustomError

--- FinalExercise/EnterpriseApplication/src/main/java/spring/springboot/EnterpriseApplication/exceptions/UnprocesableTripCustomError.java
+++ FinalExercise/EnterpriseApplication/src/main/java/spring/springboot/EnterpriseApplication/exceptions/UnprocesableTripCustomError.java
@@ -2,6 +2,7 @@ package spring.springboot.EnterpriseApplication.exceptions;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.FieldError;
 
 import javax.validation.constraints.NotNull;
 import java.util.Date;
@@ -11,6 +12,21 @@ import java.util.Date;
 public class UnprocesableTripCustomError {
 
     private String destination;
-    private Date date;
-    private Integer hour;
+    private String date;
+    private String hour;
+
+    public void setDestination(FieldError destinationMismatch) {
+        if(destinationMismatch != null)
+            this.destination = destinationMismatch.getDefaultMessage();
+    }
+
+    public void setDate(FieldError dateMismatch) {
+        if(dateMismatch != null)
+            this.date = dateMismatch.getDefaultMessage();
+    }
+
+    public void setHour(FieldError hourMismatch) {
+        if(hourMismatch != null)
+            this.hour = hourMismatch.getDefaultMessage();
+    }
 }
