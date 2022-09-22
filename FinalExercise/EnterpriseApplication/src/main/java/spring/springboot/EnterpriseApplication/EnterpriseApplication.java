bf8b40f139cff31d55e65d0989a49de7e9962947
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2022-09-22 08:43:50 +0200

    Added PostgreSQL dependency to both pom.xml

--- FinalExercise/EnterpriseApplication/src/main/java/spring/springboot/EnterpriseApplication/EnterpriseApplication.java
+++ FinalExercise/EnterpriseApplication/src/main/java/spring/springboot/EnterpriseApplication/EnterpriseApplication.java
@@ -26,11 +26,11 @@ public class EnterpriseApplication {
 	@PostConstruct
 	public void initializeDB(){
 
-		personRepository.save(new PersonEntity("71230484B", "manolo.agonzalez@bosonit.com", "ManoloContrasena", "Manolo", "Arias Gonzalez", "967823512", "ADMIN"));
-		personRepository.save(new PersonEntity("09826343K", "daniel.dmiguel@bosonit.com", "DanielContrasena", "Daniel", "Díez Miguel", "689426986", "USER"));
-		personRepository.save(new PersonEntity("09643916L", "emar.agonzalez@bosonit.com", "EmarContrasena", "Emar", "Martínez de Pedro", "609812382", "USER"));
-		personRepository.save(new PersonEntity("46328702M", "oscar.clorenzo@bosonit.com", "OscarContrasena", "Oscar", "Casado Lorenzo", "967123879", "USER"));
-		personRepository.save(new PersonEntity("28735497F", "alberto.asaavedra@bosonit.com", "AlbertoContrasena", "Alberto", "Alonso Saavedra", "763428973", "USER"));
+		personRepository.save(new PersonEntity("71230484B", "manolo.agonzalez@gmail.com", "ManoloContrasena", "Manolo", "Arias Gonzalez", "967823512", "ADMIN"));
+		personRepository.save(new PersonEntity("09826343K", "daniel.dmiguel@gmail.com", "DanielContrasena", "Daniel", "Díez Miguel", "689426986", "USER"));
+		personRepository.save(new PersonEntity("09643916L", "emar.agonzalez@gmail.com", "EmarContrasena", "Emar", "Martínez de Pedro", "609812382", "USER"));
+		personRepository.save(new PersonEntity("46328702M", "oscar.clorenzo@gmail.com", "OscarContrasena", "Oscar", "Casado Lorenzo", "967123879", "USER"));
+		personRepository.save(new PersonEntity("28735497F", "alberto.asaavedra@gmail.com", "AlbertoContrasena", "Alberto", "Alonso Saavedra", "763428973", "USER"));
 
 		tripRepository.save(new TripEntity("Barcelona", new Date(2022, 9, 6), 8));
 		tripRepository.save(new TripEntity("Valencia", new Date(2022, 9, 6), 12));
