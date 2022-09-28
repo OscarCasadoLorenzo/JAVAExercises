f22b195cc9403bb8e25c20624404c4e8c64e1686
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2022-09-28 13:08:08 +0200

    Seed DB from code removed

--- FinalExercise/EnterpriseApplication/src/main/java/spring/springboot/EnterpriseApplication/EnterpriseApplication.java
+++ FinalExercise/EnterpriseApplication/src/main/java/spring/springboot/EnterpriseApplication/EnterpriseApplication.java
@@ -22,23 +22,4 @@ public class EnterpriseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EnterpriseApplication.class, args);
 	}
-
-	@PostConstruct
-	public void initializeDB(){
-
-		personRepository.save(new PersonEntity("71230484B", "manolo.agonzalez@gmail.com", "ManoloContrasena", "Manolo", "Arias Gonzalez", "967823512", "ADMIN"));
-		personRepository.save(new PersonEntity("09826343K", "daniel.dmiguel@gmail.com", "DanielContrasena", "Daniel", "Díez Miguel", "689426986", "USER"));
-		personRepository.save(new PersonEntity("09643916L", "emar.agonzalez@gmail.com", "EmarContrasena", "Emar", "Martínez de Pedro", "609812382", "USER"));
-		personRepository.save(new PersonEntity("46328702M", "oscar.clorenzo@gmail.com", "OscarContrasena", "Oscar", "Casado Lorenzo", "967123879", "USER"));
-		personRepository.save(new PersonEntity("28735497F", "alberto.asaavedra@gmail.com", "AlbertoContrasena", "Alberto", "Alonso Saavedra", "763428973", "USER"));
-
-		tripRepository.save(new TripEntity("Barcelona", new Date(2022, 9, 6), 8));
-		tripRepository.save(new TripEntity("Valencia", new Date(2022, 9, 6), 12));
-		tripRepository.save(new TripEntity("Madrid", new Date(2022, 9, 6), 12));
-		tripRepository.save(new TripEntity("Bilbao", new Date(2022, 9, 7), 16));
-		tripRepository.save(new TripEntity("Barcelona", new Date(2022, 9, 7), 16));
-		tripRepository.save(new TripEntity("Barcelona", new Date(2022, 9, 8), 20));
-	}
-
-
 }
